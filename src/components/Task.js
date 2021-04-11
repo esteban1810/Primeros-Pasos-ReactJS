33cc356df70730fb55b1f6e8365a749adc444da0
esteban1810 <estebansevillapg@gmail.com>
2021-04-11 15:20:40 -0500

    styles

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,36 @@
 import {Component} from 'react';
+import './task.css';
+
+const button = {
+    backgroundColor: 'red',
+    borderStyle:'none',
+    borderRadius: '5px',
+    fontSize:'18px'
+}
 
 class Task extends Component {
+    parrafos(){
+        return {
+            fontFamily:'Arial'
+        }
+    }
+
+    boton(){
+        if(this.props.task.done){
+            return {
+                display:'none'
+            }
+        }
+    }
+
     render(){
         const {id,title,description,done} = this.props.task;
-        return <div key={id}>
+        return <div style={this.boton()}  key={id}>
                     <h1>{title}</h1>
-                    <p>{id}</p>
+                    <p style={this.parrafos()}>{id}</p>
                     <p>{description}</p>
                     <p>{done}</p>
+                    <button style={button}>X</button>
                 </div>
     }
 }
