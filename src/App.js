7f66d8550eaf123bbf3550dcd675fb9440c68903
esteban1810 <estebansevillapg@gmail.com>
2021-04-10 19:24:21 -0500

    components / state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// function HelloWorld({text,title}){
+//   return(
+//     <div id="hello">
+//       <h3>{title}</h3>
+//       <p>{text}</p>
+//     </div>
+//   );
+// }
+
+class HelloWorld extends React.Component {
+  state = {
+    visible: true
+  }
+
+  // ocultar(){
+  //   this.setState({visible:false});
+  // }
+
+  cambiarEstado = ()=>{
+    this.setState({visible:!this.state.visible})
+  }
+
+  render() {
+    if(this.state.visible){
+      return (
+        <div id="hello">
+          <h3>{this.props.title}</h3>
+          <p>{this.props.text}</p>
+          <button onClick={this.cambiarEstado}>OCULTAR</button>
+          {/* <button onClick={this.ocultar.bind(this)}>OCULTAR</button> */}
+          {/* <button onClick={()=>this.setState({visible:false})}>OCULTAR</button> */}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h3>No existe</h3>
+          <button onClick={this.cambiarEstado}>MOSTRAR</button>
+        </div>
+      )
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>THIS A COMPONENT: 
+      <HelloWorld text="Hola mundo" title="saludo"/> 
+      <HelloWorld text="Adios, basura" title="despedida"/> 
+
     </div>
   );
 }
